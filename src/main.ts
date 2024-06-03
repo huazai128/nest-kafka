f9b61b294d90f56e8f59f7b6145df0c4c7fb0829
zhouweihua <zhouweihua@joyy.com>
2024-06-03 14:21:27 +0800

    feat: kafka

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { kafkaOptions } from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 服务
+  const microservice = app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: kafkaOptions,
+  });
+  await app.startAllMicroservices();
   await app.listen(3000);
+
+  microservice.listen().then((port) => {
+    console.log('Microservice is listening');
+  });
 }
 bootstrap();
