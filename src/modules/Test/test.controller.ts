d5e482fe8543e3cd31c6c4428f9748773ba8cbb1
zhouweihua <zhouweihua@joyy.com>
2024-07-16 19:07:14 +0800

    feat: kafka测试

--- src/modules/Test/test.controller.ts
+++ src/modules/Test/test.controller.ts
@@ -1,31 +1,37 @@
 import { Controller, Get } from '@nestjs/common';
 import { TestService, Item } from './test.service';
-import { Observable } from 'rxjs';
-import {
-  Ctx,
-  KafkaContext,
-  MessagePattern,
-  Payload,
-} from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { MONITOR_TOPIC } from 'config';
 
 @Controller()
 export class TestController {
   constructor(private readonly kafkaService: TestService) {}
 
+  private fibonacci(n: number) {
+    return n < 1
+      ? 0
+      : n <= 2
+        ? 1
+        : this.fibonacci(n - 1) + this.fibonacci(n - 2);
+  }
+
+  @Get('test1')
+  getTestKafka(): string {
+    const value = this.fibonacci(40);
+    return value;
+  }
+
   @Get('test')
-  getHello(): string {
-    Array.from({ length: 1000 }).map((item, index) => {
-      this.kafkaService.sendMessage({
-        id: index,
-        name: `kafka-${index}`,
-      });
+  getTest(): string {
+    this.kafkaService.sendMessage({
+      id: 40,
+      name: `kafka`,
     });
     return '发送成功';
   }
 
   @MessagePattern(MONITOR_TOPIC)
-  handleMessage(@Payload() data: Item, @Ctx() context: KafkaContext) {
-    console.log(data, 'data========');
+  handleMessage(@Payload() data: Item) {
+    return this.fibonacci(data.id);
   }
 }
